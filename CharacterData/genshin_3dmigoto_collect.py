65d6b026058b8a573d3c5a9931a825a93de7858c
SilentNightSound <107697535+SilentNightSound@users.noreply.github.com>
2022-06-18 20:34:25 -0400

    Adding character data and scripts

---
+++ CharacterData/genshin_3dmigoto_collect.py
@@ -0,0 +1,299 @@
+# Author: SilentNightSound#7430
+# Collects and organizes the relevant character model data from a frame dump, outputting it in a format
+# that the Blender 3Dmigoto plugin recognizes
+
+# Currently only works for player characters, and not yet tested on every character
+
+import os
+import sys
+import argparse
+import shutil
+import json
+
+def main():
+    parser = argparse.ArgumentParser(description="Collects, organizes and compiles character buffer data from frame dumps")
+    parser.add_argument("-vb", type=str, help="Main VB character is drawn on")
+    parser.add_argument("-n", "--name", type=str, help="Name of character to use in folders and output files")
+    parser.add_argument("-f", "--framedump", type=str, help="Name of framedump folder")
+    parser.add_argument("-vs", type=str, default="653c63ba4a73ca8b", help="Root VS for character model")
+    parser.add_argument("--force", nargs="+", help="Force parser to use specified ids")
+    args = parser.parse_args()
+
+
+    root_vs_hash = args.vs
+    draw_vb_hash = args.vb
+    frame_dump = args.framedump
+    character = args.name
+
+    object_classifications = {0: "Head", 1: "Body", 2: "Extra"}
+
+    position_vb = ""
+    blend_vb = ""
+    texcoord_vb = ""
+    first_vs = ""
+
+    relevant_ids = []
+
+    print("Creating output folder")
+    if not os.path.isdir(character):
+        os.mkdir(character)
+    else:
+        print(f"WARNING: Everything currently in the {args.name} folder will be overwritten - make sure any important files are backed up. Press any button to continue")
+        input()
+
+    frame_dump_files = os.listdir(frame_dump)
+
+    # First pass to give us the VB for the position and blend data, as well as identify the relevant draw ids
+    print("Searching for VB corresponding with root VS")
+    for filename in frame_dump_files:
+        if root_vs_hash in filename and "-vb0=" in filename and os.path.splitext(filename)[1] == ".txt":
+            print(f"Found position VB: {filename}")
+            if position_vb:
+                print("ERROR: found two character objects in frame dump. Exiting")
+                print(position_vb, filename)
+                return
+            position_vb = filename
+        if root_vs_hash in filename and "-vb1=" in filename and os.path.splitext(filename)[1] == ".txt":
+            print(f"Found blend VB: {filename}")
+            if blend_vb:
+                print("ERROR: found two character objects in frame dump. Exiting")
+                print(blend_vb, filename)
+                return
+            blend_vb = filename
+
+        if draw_vb_hash in filename:
+            draw_id = filename.split("-")[0]
+            if not first_vs and int(draw_id) > 10:
+                first_vs = filename.split("vs=")[1].split("-")[0]
+                print(f"Found first VS: {first_vs}")
+            # The low ids that correspond to the object are a bit weird and I don't fully understand them yet
+            # The higher ids should have all the info we need
+            if draw_id not in relevant_ids and int(draw_id)>10:
+                relevant_ids.append(draw_id)
+
+    if not position_vb or not blend_vb:
+        print("ERROR: Unable to find root position and blend VB. Exiting")
+        return
+
+    print(f"Relevant IDs: {relevant_ids}")
+
+    # Second pass to collect IBs, texcoord VB, and diffuse/light maps
+    model_data = {}
+    previous_id = relevant_ids[0]
+    texcoord_largest_size = -1
+
+    # A couple of characters have unique draw call formats, this forces a specific list of ids to be parsed
+    if args.force:
+        print(f"Ignoring found IDs and forcing analysis on IDs {args.force}")
+        relevant_ids = args.force
+    for current_id in relevant_ids:
+
+        # Draw calls for characters either come in pairs or triples
+        if not args.force and int(current_id) - int(previous_id) > 1:
+            print("Finished collecting information")
+            break
+
+        # Collecting IB file for portion of model being drawn on the current id
+        current_id_files = [name for name in frame_dump_files if f"{current_id}-" in name]
+        draw_ib = [name for name in current_id_files if "-ib=" in name and os.path.splitext(name)[1] == ".txt"]
+        if len(draw_ib) != 1:
+            print(f"ERROR: Unable to find corresponding IB for draw ID {current_id}. Exiting")
+            return
+        draw_ib = draw_ib[0]
+
+        with open(os.path.join(frame_dump, draw_ib), "r") as f:
+            first_index = -1
+            for line in f.readlines():
+                if "first index:"in line:
+                    first_index = int(line.split(":")[1].strip())
+                    break
+            if first_index < 0:
+                print("ERROR: Unrecognized IB format. Exiting")
+                return
+        print(f"ID: {current_id}, found object at index {first_index}")
+        model_data[first_index] = [draw_ib]
+
+        # Even though each object technically has its own texcoord, they are all just portions of a single vb
+        # The largest file will always contain the full texcoord for every point
+        texcoord_vb_candidate = [name for name in current_id_files if "-vb1=" in name and os.path.splitext(name)[1] == ".txt"]
+        if len(texcoord_vb_candidate) != 1:
+            print(f"ERROR: Unable to find corresponding texcoord information for draw ID {current_id}. Exiting")
+            return
+        texcoord_vb_candidate = texcoord_vb_candidate[0]
+
+        texcoord_size = os.stat(os.path.join(frame_dump, texcoord_vb_candidate)).st_size
+        if texcoord_size > texcoord_largest_size:
+            texcoord_largest_size = texcoord_size
+            texcoord_vb = texcoord_vb_candidate
+
+        # Finally, collect textures
+        # As far as I can tell, all object models have two each though they might not be unique
+        # I don't know what the higher numbered textures are for, but they don't seem to have any impact
+        texture_maps = [name for name in current_id_files if "-ps-t" in name and os.path.splitext(name)[1] == ".dds"]
+        if len(texture_maps)<2:
+            print(f"ERROR: Unable to find diffuse and lightmaps for {current_id}. Exiting")
+            return
+
+        model_data[first_index].append(texture_maps[0])
+        model_data[first_index].append(texture_maps[1])
+
+        previous_id = current_id
+
+    if len(model_data.keys()) < 2:
+        print(f"ERROR: Unable to find all model components. Only found: {model_data.keys()}. Exiting")
+        return
+
+    # Collecting this information from the .buf is simpler, but we lose out on the header information
+    # Order is POSITION (R32G32B32_FLOAT), NORMAL (R32G32B32_FLOAT), TANGENT (R32G32B32A32_FLOAT)
+    # Sizes are 12, 12, 16 for a total stride of 40
+    # All other values ignored
+    print("Collecting position data")
+    position = []
+    with open(os.path.join(frame_dump, position_vb), "r") as f:
+        headers, data = f.read().split("vertex-data:\n")
+        data = data.strip().split("\n")
+        # These are not the colors and texcoord we want
+        data = [line for line in data if "COLOR:" not in line and "TEXCOORD" not in line and line]
+        vertex_group = []
+        for i in range(len(data)):
+            vertex = data[i].split(":")[1].strip().split(", ")
+            vertex_group.append(vertex)
+            i += 1
+            if i%3 == 0:
+                position.append(vertex_group)
+                vertex_group = []
+
+    # Order is BLENDWEIGHT (R32G32B32A32_FLOAT), BLENDINDICES (R32G32B32A32_SINT)
+    # Sizes are 16,16 for a total stride of 32
+    print("Collecting blend data")
+    blend = []
+    with open(os.path.join(frame_dump, blend_vb), "r") as f:
+        headers, data = f.read().split("vertex-data:\n")
+        data = data.strip().split("\n")
+        data = [line for line in data if line]
+        vertex_group = []
+        for i in range(len(data)):
+            vertex = data[i].split(":")[1].strip().split(", ")
+            vertex_group.append(vertex)
+            i += 1
+            if i%2 == 0:
+                blend.append(vertex_group)
+                vertex_group = []
+
+    # Some characters have one texcoord, others have two, varies from character to character
+    # Order is COLOR (R8G8B8A8_UNORM), TEXCOORD (R32G32_FLOAT), *TEXCOORD1 (R32G32_FLOAT) if it exists
+    # Sizes are 4, 8, *8 for a total of either 12 or 20
+    print("Collecting texcoord data")
+    texcoord = []
+    with open(os.path.join(frame_dump, texcoord_vb), "r") as f:
+        headers, data = f.read().split("vertex-data:\n")
+        data = data.strip().split("\n")
+        if "stride: 20" in headers:
+            tex_group_count = 3
+        else:
+            tex_group_count = 2
+            data = [line for line in data if "TEXCOORD1" not in line]
+        data = [line for line in data if line]
+        vertex_group = []
+        for i in range(len(data)):
+            vertex = data[i].split(":")[1].strip().split(", ")
+            vertex_group.append(vertex)
+            i += 1
+            if i % tex_group_count == 0:
+                texcoord.append(vertex_group)
+                vertex_group = []
+
+    if len(position) != len(blend) or len(blend) != len(texcoord):
+        print(f"Error: Size mismatch between buffers. Position: {len(position)}, Blend: {len(blend)}, Texcoord: {len(texcoord)}. Exiting")
+
+        return
+
+    # TODO: load these headers from a separate file, or dynamically calculate them from the original .txt buffers
+    print("Constructing combined buffer")
+    vb_merged = ""
+    if tex_group_count == 3:
+        vb_merged += "stride: 92\n"
+    else:
+        vb_merged += "stride: 84\n"
+    vb_merged += f"first vertex: 0\nvertex count: {len(position)}\ntopology: trianglelist\n"
+    vb_merged += "element[0]:\n  SemanticName: POSITION\n  SemanticIndex: 0\n  Format: R32G32B32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 0\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[1]:\n  SemanticName: NORMAL\n  SemanticIndex: 0\n  Format: R32G32B32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 12\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[2]:\n  SemanticName: TANGENT\n  SemanticIndex: 0\n  Format: R32G32B32A32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 24\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[3]:\n  SemanticName: BLENDWEIGHT\n  SemanticIndex: 0\n  Format: R32G32B32A32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 40\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[4]:\n  SemanticName: BLENDINDICES\n  SemanticIndex: 0\n  Format: R32G32B32A32_SINT\n  InputSlot: 0\n  AlignedByteOffset: 56\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[5]:\n  SemanticName: COLOR\n  SemanticIndex: 0\n  Format: R8G8B8A8_UNORM\n  InputSlot: 0\n  AlignedByteOffset: 72\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    vb_merged += "element[6]:\n  SemanticName: TEXCOORD\n  SemanticIndex: 0\n  Format: R32G32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 76\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+    if tex_group_count == 3:
+        vb_merged += "element[7]:\n  SemanticName: TEXCOORD\n  SemanticIndex: 1\n  Format: R32G32_FLOAT\n  InputSlot: 0\n  AlignedByteOffset: 84\n  InputSlotClass: per-vertex\n  InstanceDataStepRate: 0\n"
+
+    vb_merged += "\nvertex-data:\n\n"
+    for i in range(len(position)):
+        vb_merged += f"vb0[{i}]+000 POSITION: " + ", ".join(position[i][0]) + "\n"
+        vb_merged += f"vb0[{i}]+012 NORMAL: " + ", ".join(position[i][1]) + "\n"
+        vb_merged += f"vb0[{i}]+024 TANGENT: " + ", ".join(position[i][2]) + "\n"
+        vb_merged += f"vb0[{i}]+040 BLENDWEIGHT: " + ", ".join(blend[i][0]) + "\n"
+        vb_merged += f"vb0[{i}]+040 BLENDINDICES: " + ", ".join(blend[i][1]) + "\n"
+        vb_merged += f"vb0[{i}]+072 COLOR: " + ", ".join(texcoord[i][0]) + "\n"
+        vb_merged += f"vb0[{i}]+076 TEXCOORD: " + ", ".join(texcoord[i][1]) + "\n"
+        if len(texcoord[i]) == 3:
+            vb_merged += f"vb0[{i}]+084 TEXCOORD1: " + ", ".join(texcoord[i][2]) + "\n"
+        vb_merged += "\n"
+
+    vb_merged = vb_merged[:-1]
+
+    # Now that we have all the buffer data, assemble the final output
+    print("Constructing final buffers")
+    position_vb_hash = position_vb.split("=")[1].split("-")[0]
+    sorted_indexes = sorted(model_data.keys())
+    count = 0
+    for index in sorted_indexes:
+        classification = object_classifications[count]
+        print(f"{classification} object at index {index}")
+        print(f"Relevant files: {model_data[index]}")
+        with open(os.path.join(character, f"{character}{classification}-vb0={position_vb_hash}.txt"), "w") as f:
+            f.write(vb_merged)
+
+        ib_hash = model_data[index][0].split("=")[1].split("-")[0]
+        shutil.copyfile(os.path.join(frame_dump, model_data[index][0]), os.path.join(character, f"{character}{classification}-ib={ib_hash}.txt"))
+        shutil.copyfile(os.path.join(frame_dump, model_data[index][1]), os.path.join(character, f"{character}{classification}Diffuse.dds"))
+        shutil.copyfile(os.path.join(frame_dump, model_data[index][2]),os.path.join(character, f"{character}{classification}LightMap.dds"))
+        count += 1
+
+
+    # Will need this info later to construct .ini file
+    if "hash_info.json" not in os.listdir(".") or os.stat("hash_info.json").st_size == 0:
+        f = open("hash_info.json", "w")
+        hash_data = {}
+    else:
+        f = open("hash_info.json", "r+")
+        hash_data = json.load(f)
+
+    if character in hash_data:
+        print("Character already exists in hash file, skipping")
+        print("All operations completed, exiting")
+        return
+
+    print("Adding character hash info to hash file")
+    position_vb_hash = position_vb.split("=")[1].split("-")[0]
+    blend_vb_hash = blend_vb.split("=")[1].split("-")[0]
+    texcoord_vb_hash = texcoord_vb.split("=")[1].split("-")[0]
+
+    hash_data[character] = {}
+    hash_data[character]["root_vs"] = root_vs_hash
+    hash_data[character]["draw_vb"] = draw_vb_hash
+    hash_data[character]["position_vb"] = position_vb_hash
+    hash_data[character]["blend_vb"] = blend_vb_hash
+    hash_data[character]["texcoord_vb"] = texcoord_vb_hash
+    hash_data[character]["ib"] = ib_hash
+    hash_data[character]["object_indexes"] = sorted_indexes
+    hash_data[character]["first_vs"] = first_vs
+
+    f.seek(0)
+    json.dump(hash_data, f)
+    f.truncate()
+
+    print("All operations completed, exiting")
+
+
+if __name__ == "__main__":
+    main()
---
+++ CharacterData/genshin_3dmigoto_generate.py
@@ -0,0 +1,175 @@
+# Author: SilentNightSound#7430
+# Splits the output of the 3dmigoto blender plugin into the required buffers, along with generating
+# the required .ini file for performing the override
+
+import os
+import argparse
+import struct
+import shutil
+import json
+
+def main():
+    parser = argparse.ArgumentParser(description="Splits blender 3dmigoto output into required buffers")
+    parser.add_argument("-n", "--name", type=str, help="Name of character to use in folders and output files")
+    parser.add_argument("--hashfile", type=str, default="hash_info.json", help="Name of hash info file")
+    args = parser.parse_args()
+
+    if args.hashfile not in os.listdir("."):
+        print(f"ERROR: Cannot find {args.hashfile}. Please ensure it is in the same folder. Exiting")
+        return
+
+    with open(args.hashfile, "r") as f:
+        hash_data = json.load(f)
+        if args.name not in hash_data:
+            print(f"ERROR: Character hash information not found in {args.hashfile}. Please either manually add hash information or use genshin_3dmigoto_collect.py to generate it from a frame dump. Exiting")
+            return
+
+        char_hashes = hash_data[args.name]
+
+    if not os.path.isdir(args.name):
+        print(f"ERROR: Unable to find {args.name} folder. Ensure the folder exists. Exiting")
+        return
+
+    if f"{args.name}Head.vb" not in os.listdir(args.name):
+        print(f"ERROR: Unable to find {args.name}Head.vb. Ensure it exists in the {args.name} folder. Exiting")
+        return
+
+    if not os.path.isdir(f"{args.name}Mod"):
+        print(f"Creating {args.name}Mod")
+        os.mkdir(f"{args.name}Mod")
+    else:
+        print(f"WARNING: Everything currently in the {args.name}Mod folder will be overwritten - make sure any important files are backed up. Press any button to continue")
+        input()
+
+
+    # TODO: actually use these headers instead of just hardcoding values
+    # Need to check the format to see if there are two texcoord or just one
+    with open(os.path.join(args.name, f"{args.name}Head.fmt"), "r") as f:
+        headers = f.read()
+        if "stride: 84" in headers:
+            extra_flag = False
+        else:
+            extra_flag = True
+
+    print("Splitting VB by buffer type, merging body parts")
+    position, blend, texcoord = collect_vb(args.name, "Head", extra_flag)
+    ib = collect_ib(args.name, "Head", 0)
+    while len(ib)/2 < char_hashes['object_indexes'][1]:
+        ib +=  struct.pack('1h', 0)
+
+    if len(position)%40 != 0:
+        print("ERROR: VB buffer length does not match stride")
+    body_start_offset = len(position)//40
+    x,y,z = collect_vb(args.name, "Body", extra_flag)
+    position += x
+    blend += y
+    texcoord += z
+    ib += collect_ib(args.name, "Body", body_start_offset)
+    if extra_flag:
+        while len(ib) / 2 < char_hashes['object_indexes'][2]:
+            ib += struct.pack('1h', 0)
+        extra_start_offset = len(position)//40
+        x, y, z = collect_vb(args.name, "Extra", extra_flag)
+        position += x
+        blend += y
+        texcoord += z
+        ib += collect_ib(args.name, "Extra", extra_start_offset)
+
+
+
+    print("Writing merged buffer files")
+    with open(os.path.join(f"{args.name}Mod", f"{args.name}Position.buf"), "wb") as f, \
+            open(os.path.join(f"{args.name}Mod", f"{args.name}Blend.buf"), "wb") as g, \
+            open(os.path.join(f"{args.name}Mod", f"{args.name}Texcoord.buf"), "wb") as h:
+        f.write(position)
+        g.write(blend)
+        h.write(texcoord)
+
+    with open(os.path.join(f"{args.name}Mod", f"{args.name}.ib"), "wb") as f:
+        f.write(ib)
+
+    print("Copying texture files")
+    shutil.copy(os.path.join(args.name, f"{args.name}HeadDiffuse.dds"), os.path.join(f"{args.name}Mod", f"{args.name}HeadDiffuse.dds"))
+    shutil.copy(os.path.join(args.name, f"{args.name}HeadLightMap.dds"), os.path.join(f"{args.name}Mod", f"{args.name}HeadLightMap.dds"))
+    shutil.copy(os.path.join(args.name, f"{args.name}BodyDiffuse.dds"), os.path.join(f"{args.name}Mod", f"{args.name}BodyDiffuse.dds"))
+    shutil.copy(os.path.join(args.name, f"{args.name}BodyLightmap.dds"), os.path.join(f"{args.name}Mod", f"{args.name}BodyLightMap.dds"))
+    if extra_flag:
+        shutil.copy(os.path.join(args.name, f"{args.name}ExtraDiffuse.dds"),
+                    os.path.join(f"{args.name}Mod", f"{args.name}ExtraDiffuse.dds"))
+        shutil.copy(os.path.join(args.name, f"{args.name}ExtraLightMap.dds"),
+                    os.path.join(f"{args.name}Mod", f"{args.name}ExtraLightMap.dds"))
+
+
+    # TODO: Populate template file instead of hardcoded
+    print("Generating .ini file")
+    ini_data = f"; {args.name}\n\n"
+
+    ini_data += f"; Overrides -------------------------\n\n"
+    ini_data += f"[TextureOverride{args.name}Position]\nhash = {char_hashes['position_vb']}\nvb0 = Resource{args.name}Position\n\n"
+    ini_data += f"[TextureOverride{args.name}Blend]\nhash = {char_hashes['blend_vb']}\nvb1 = Resource{args.name}Blend\n\n"
+    ini_data += f"[TextureOverride{args.name}Texcoord]\nhash = {char_hashes['texcoord_vb']}\nvb1 = Resource{args.name}Texcoord\n\n"
+    ini_data += f"[TextureOverride{args.name}IB]\nhash = {char_hashes['ib']}\nib = Resource{args.name}IB\n\n"
+    ini_data += f"[TextureOverride{args.name}Head]\nhash = {char_hashes['ib']}\nmatch_first_index = {char_hashes['object_indexes'][0]}\nps-t0 = Resource{args.name}HeadDiffuse\nps-t1 = Resource{args.name}HeadLightMap\n\n"
+    ini_data += f"[TextureOverride{args.name}Body]\nhash = {char_hashes['ib']}\nmatch_first_index = {char_hashes['object_indexes'][1]}\nps-t0 = Resource{args.name}BodyDiffuse\nps-t1 = Resource{args.name}BodyLightMap\n\n"
+    if extra_flag:
+        ini_data += f"[TextureOverride{args.name}Extra]\nhash = {char_hashes['ib']}\nmatch_first_index = {char_hashes['object_indexes'][2]}\nps-t0 = Resource{args.name}ExtraDiffuse\nps-t1 = Resource{args.name}ExtraLightMap\n\n"
+
+    ini_data += f"; Resources -------------------------\n\n"
+    ini_data += f"[Resource{args.name}Position]\ntype = Buffer\nStride = 40\nfilename = {args.name}Position.buf\n\n"
+    ini_data += f"[Resource{args.name}Blend]\ntype = Buffer\nStride = 32\nfilename = {args.name}Blend.buf\n\n"
+    if extra_flag:
+        ini_data += f"[Resource{args.name}Texcoord]\ntype = Buffer\nStride = 20\nfilename = {args.name}Texcoord.buf\n\n"
+    else:
+        ini_data += f"[Resource{args.name}Texcoord]\ntype = Buffer\nStride = 12\nfilename = {args.name}Texcoord.buf\n\n"
+
+    ini_data += f"[Resource{args.name}IB]\ntype = Buffer\nFormat = DXGI_FORMAT_R16_UINT\nfilename = {args.name}.ib\n\n"
+    ini_data += f"[Resource{args.name}HeadDiffuse]\nfilename = {args.name}HeadDiffuse.dds\n\n"
+    ini_data += f"[Resource{args.name}HeadLightMap]\nfilename = {args.name}HeadLightMap.dds\n\n"
+    ini_data += f"[Resource{args.name}BodyDiffuse]\nfilename = {args.name}BodyDiffuse.dds\n\n"
+    ini_data += f"[Resource{args.name}BodyLightMap]\nfilename = {args.name}BodyLightMap.dds\n\n"
+    if extra_flag:
+        ini_data += f"[Resource{args.name}ExtraDiffuse]\nfilename = {args.name}ExtraDiffuse.dds\n\n"
+        ini_data += f"[Resource{args.name}ExtraLightMap]\nfilename = {args.name}ExtraLightMap.dds\n\n"
+
+    with open(os.path.join(f"{args.name}Mod", f"{args.name}.ini"), "w") as f:
+        print("Writing ini file")
+        f.write(ini_data)
+
+    print("All operations completed, exiting")
+
+
+
+def collect_vb(name, classification, extra_flag):
+    position = bytearray()
+    blend = bytearray()
+    texcoord = bytearray()
+    with open(os.path.join(name, f"{name}{classification}.vb"), "rb") as f:
+        data = f.read()
+        data = bytearray(data)
+        i = 0
+        while i < len(data):
+            position += data[i:i+40]
+            blend += data[i+40:i+72]
+            if extra_flag:
+                texcoord += data[i+72:i+92]
+                i += 92
+            else:
+                texcoord += data[i+72:i+84]
+                i += 84
+    return position, blend, texcoord
+
+
+def collect_ib(name, classification, offset):
+    ib = bytearray()
+    with open(os.path.join(name, f"{name}{classification}.ib"), "rb") as f:
+        data = f.read()
+        data = bytearray(data)
+        i = 0
+        while i < len(data):
+            ib += struct.pack('1h', struct.unpack('1h', data[i:i+2])[0]+offset)
+            i += 2
+    return ib
+
+
+if __name__ == "__main__":
+    main()
